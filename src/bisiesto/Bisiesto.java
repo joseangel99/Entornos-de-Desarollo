6d1c49790438ec67e66393bd1a88cb3c21a5cbb4
joseangel99 <33617922+joseangel99@users.noreply.github.com>
2017-11-13 09:02:16 +0100

    Update Bisiesto.java

--- src/bisiesto/Bisiesto.java
+++ src/bisiesto/Bisiesto.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bisiesto;
 import java.util.Scanner;
-/**
- * para encontrar el doc, tenemos que ir a la carpeta del proyecto/ dis/ doc/ y abrir el archivo index
- * @author Yo mismo
- * @version 1.0 07/11/2017
- */
-/*
-* @param Le damos anio, ara saber si es bisiesto o no
-* @return Devuelte tru si anio es bisiesto, sino devuelve false
-*/
+
 public class Bisiesto {
     public static boolean esBisiesto(int anio){
        if(anio % 400 == 0 || anio % 4 == 0 && anio % 100 != 0){
